cd5a3fd3a66f9db538fcf3cec591b16144a101b1
kang-kyoungmin <rudals1173@naver.com>
2023-06-05 13:23:32 +0900

    할당 연산자

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+# 할당 연산자 =
+a = 20
+b = 20
+m = 15
 
+y = a + b
+print(y)
 
+m += 10 # m = m + 10
+print(m)
 
-# 논리 연산자
-a = 5
-b = 2
-c = 3
-d = 200
+m **= 2
+print(m)
 
-print('AND 연산자')
-print('a > b and a < c:', a > b and a > c)
-
-print("OR 연산자")
-print('a > b or a < c:', a > b or a < c)
-
-print('NOT 연산자')
-print('not(a < b): ', not(a < b))
-
+m //=10
+print(m)
