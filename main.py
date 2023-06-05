cbcfe23ac9bf6a96bc35c1cf9d7c4730233b3ee2
kang-kyoungmin <rudals1173@naver.com>
2023-06-05 13:19:57 +0900

    논리 연산자

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+# 논리 연산자
+a = 5
+b = 2
+c = 3
+d = 200
+
+print('AND 연산자')
+print('a > b and a < c:', a > b and a > c)
+
+print("OR 연산자")
+print('a > b or a < c:', a > b or a < c)
+
+print('NOT 연산자')
+print('not(a < b): ', not(a < b))
+
