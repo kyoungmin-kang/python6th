9cb3e6127e1c08673360fb7f63d216411dbb2639
kang-kyoungmin <rudals1173@naver.com>
2023-06-05 15:24:11 +0900

    출력 함수 실습

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("1")
-print("2", end='')
-print("3")
-print("4")
+data = [10, 20, -50, 21.3, 'Like Lion']
+print(data)
+
+print("Like", "Share", "Subcribe", sep='')
+print("Like", "Share", "Subcribe", sep='***')
+
+print("Like", "Share", "Subcribe", sep='***', end='\t') # \t(특수문자)=tap(들여쓰기 같은 띄어쓰기), \n(기본형 특수문자)=줄바꿈
+print("Like", "Share", "Subcribe", sep='***', end='\n')
+
+m = 40
+print("value: ", m)
+
+name = "강경민"
+age = 28
+print("My name is", name, "and My age is", age, sep=' ')
+
+
