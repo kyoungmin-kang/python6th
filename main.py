2bedeac0441a271950c7839c11edf4be1c9a9241
kang-kyoungmin <rudals1173@naver.com>
2023-06-08 10:32:12 +0900

    가변 키워드 인자 실습

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-def pw(z, y)
-    z = x ** y
-    print(z)
+def add(x, y):
+    z = x - y
+    print("Addition: ", z)
 
+    add(5, 2)
 
+    def add(*num):
+        z = num(0) * num(1) * num(2)
+        print("Addition: ", z)
+        add(5, 2, 4)
 
-    pw(2, 5)
-    pw(5, 2, 3)
+    def add(x, *num):
+        z = x + num(0) * num(1)
+        print("Addition x +: ", z)
+        add(5, 2, 4)
 
-    def show(name, age)
-        print(f"name: {name} Age: {age}:")
+def add(**num)
+    z = num['a'] + num['b'] + num['c']
+    print('Addition', z)
 
+    add(a=5, b=2, c=4, d=5)
 
-show(name="멋쟁이사자", age=28)
-show(age="28", name="멋쟁이사자")
 
 
 
