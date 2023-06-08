4015d56666b85ea131e009b5a39a6355fc492059
kang-kyoungmin <rudals1173@naver.com>
2023-06-08 10:39:20 +0900

    지역 변수와 전역 변수 실습

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-def add(x, y):
-    z = x - y
-    print("Addition: ", z)
+def show():
+    x = 10
+    print(x)
 
-    add(5, 2)
 
-    def add(*num):
-        z = num(0) * num(1) * num(2)
-        print("Addition: ", z)
-        add(5, 2, 4)
+    show()
 
-    def add(x, *num):
-        z = x + num(0) * num(1)
-        print("Addition x +: ", z)
-        add(5, 2, 4)
 
-def add(**num)
-    z = num['a'] + num['b'] + num['c']
-    print('Addition', z)
+    def add(y):
+        x = 10
+        print(x)
+        print(x+y)
 
-    add(a=5, b=2, c=4, d=5)
+
+        add(20)
+
+        a = 50
+
+        def show():
+            x = 10
+            print(x)
+            print(a)
+
+
+
+            print("Goval Valiable a:", a)
+            i = 0
+
+            def myfun():
+                a = i + 1
+                print("My Function", a)
+                myfun()
+                print("Goval Valiable a:", a)
+
 
 
 
