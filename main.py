9a1c0a94131ec057df906ee0944eef8e34c237f4
kang-kyoungmin <rudals1173@naver.com>
2023-06-08 15:47:48 +0900

    재귀호출

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-def val(lst):
-    print("Inside Function Before Append:", lst, id(lst))
-    lst.append(4)
-    print("Inside Function After Append:", lst, id(lst))
-
-
-list = [1, 2, 3]
-print("Before Calling Function: ", list, id(list))
-val(list)
-print("After Calling Function: ", list, id(list))
-
-
 def val(x):
     print("Inside :", x, id(x))
     x += 1
     print("Inside After:", x, id(x))
-print("========")
+import sys
+print("default:", sys.getrecursionlimit())
+sys.setrecursionlimit(3000)
+print("setting:", sys.getrecursionlimit())
+i = 0
+def myfun():
+    global i
+    i += 1
+    print("My Function:", i)
+    myfun()
+
+print("=====")
 x = 10
-print("Before Calling:", x,x id(x))
+print("Before Calling:", x, id(x))
 val(x)
-print("After Calling:", x,x id(x))
+print("After Calling: ", x, id(x))
+myfun()
+
+
 
