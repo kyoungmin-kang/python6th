9a26373bf9038ff2ac248118c5548c9a687292ff
kang-kyoungmin <rudals1173@naver.com>
2023-06-09 09:47:55 +0900

    페이스 네임슽페이스, 인스턴스,네임스페이스

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-show = lambda x: print(x)
-def decor(fun):
-    def inner():
-        a = fun()
-        add = a + 5
-        return add
+class Mobile:
+    fp = 'yes'
 
-show(5)
-    return inner
 
-add = lambda x, y: (x + y)
+realme = Mobile()
+redme = Mobile()
+geek = Mobile()
 
-print(add(5, 2))
-def num():
-    return 10
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
 
-add_sub = lambda x, y: (x + y, x - y)
-a, s = add_sub(5, 2)
+Mobile.fp = 'no'
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
+print('----------')
+realme.fp = 'Not Working'
+print(Mobile.fp)
+print(realme.fp)
+print(redme.fp)
+print(geek.fp)
 
-print(a)
-print(s)
-result_fun = decor(num)
-print(result_fun())
-
-add = lambda x, y=3: (x + y)
-
-print(add(5))
-@decor
-def num():
-    return 10
-
-print(num())
