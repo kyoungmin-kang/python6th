08a118d6e1c84a590d50fd474c2467b8f81f6399
kang-kyoungmin <rudals1173@naver.com>
2023-06-15 10:29:09 +0900

    leet-316.Remove Duplicate Letters 풀이2

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        if not  s:
+        # if not  s:
+        #     return ""
+        #
+        # for char in sorted(set(s)):
+        #     suffix = s[s.index(char):]
+        #     if set(s) == set(suffix):
+        #         return  char + self.removeDuplicateLetters(suffix.replace(char, ""))
+        # return ""
+        if not s:
             return ""
+        #문자열의 문자 빈도수 계산
+        counter = {char: 0 for char in set(s)}
 
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
-            if set(s) == set(suffix):
-                return  char + self.removeDuplicateLetters(suffix.replace(char, ""))
-        return ""
+        for char in s:
+            counter[char] += 1
+
+        #사전순으로 가장 작은 문자의 인뎃스 찾기
+        min_idx = 0
+        for i, char in enumerate(s):
+            if char < s[min_idx]:
+                min_idx = i
+            counter[char] -= 1
+            if counter[char] == 0:
+                break
+        return s[min_idx] + self.removeDuplicateLetters(s[min_idx + 1:].replace(s[min_idx], ""))
