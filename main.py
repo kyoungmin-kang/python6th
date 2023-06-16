fd111a940794896fd3ef61d473c1edff94923ccb
kang-kyoungmin <rudals1173@naver.com>
2023-06-16 11:38:54 +0900

    leet-706.Design Hashmap

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
-class Solution:
-    def mergeKLists(self, lists: List[optional[Listnode]]) -> Optional[Listnode]:
-        root = result = Listnode(Node)
-        heap = []
+class ListNode:
+    def __init__(self,key=None,value=None):
+        self.key = key
+        self.value = value
+        self.next = None
 
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i, lists[i]))
 
-        while heap:
-            node = heapq.heappop(heap)
-            idx = node[1]
-            result.next = node[2]
 
-            result = result.next
-            if result.next:
-                heapq.heappush(heap, (result.next.val, idx, result.next))
-        return  root.next
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.table = collections.dafaultdict(ListNode)
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
+    def get(self, key: int) -> int:
+        index = key % self.size
+        if self.tavle[index] is None:
+            return -1
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+            return -1
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            return
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p,p.next
