4b9d142be87eb04c7d577f5acdb703c98635a805
kang-kyoungmin <rudals1173@naver.com>
2023-06-16 11:43:13 +0900

    leet-771.Jewels and Stones

--- main.py
+++ main.py
@@ -1,53 +1,16 @@
-class ListNode:
-    def __init__(self,key=None,value=None):
-        self.key = key
-        self.value = value
-        self.next = None
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = {}
+        count = 0
 
+        for char in stones:
+            if char not in freqs:
+                freqs[char] = 1
+            else:
+                freqs[char] += 1
 
+        for char in jewels:
+            if char in freqs:
+                count += freqs[char]
 
-class MyHashMap:
-
-    def __init__(self):
-        self.size = 1000
-        self.table = collections.dafaultdict(ListNode)
-
-    def put(self, key: int, value: int) -> None:
-        index = key % self.size
-        if self.table[index].value is None:
-            self.table[index] = ListNode(key, value)
-            return
-        p = self.table[index]
-        while p:
-            if p.key == key:
-                p.value = value
-                return
-            if p.next is None:
-                break
-            p = p.next
-        p.next = ListNode(key, value)
-    def get(self, key: int) -> int:
-        index = key % self.size
-        if self.tavle[index] is None:
-            return -1
-        p = self.table[index]
-        while p:
-            if p.key == key:
-                return p.value
-            p = p.next
-            return -1
-    def remove(self, key: int) -> None:
-        index = key % self.size
-        if self.table[index].value is None:
-            return
-        p = self.table[index]
-        if p.key == key:
-            self.table[index] = ListNode() if p.next is None else p.next
-            return
-
-        prev = p
-        while p:
-            if p.key == key:
-                prev.next = p.next
-                return
-            prev, p = p,p.next
+        return count
