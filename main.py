12fab65a7737cbed82887b7279b87878ad18dd24
kang-kyoungmin <rudals1173@naver.com>
2023-06-20 09:41:10 +0900

    leet-17. Letter Combinations of a Phone Number

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = {}
-        count = 0
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(index, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
 
-        for char in stones:
-            if char not in freqs:
-                freqs[char] = 1
-            else:
-                freqs[char] += 1
+            for i in range(index, len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
 
-        for char in jewels:
-            if char in freqs:
-                count += freqs[char]
+        if not  digits:
+            return  []
 
-        return count
+        dic = {"2": "abc", "3": "def", "4": "ghi",
+               "5": "jkl", "6": "mno", "7": "pqrs",
+               "8": "tuv", "9": "wxyz"}
+        result = []
+        dfs(0, "")
+        return result
