b52b709f27ac5272e5aa886da6785c6f7df90aac
kang-kyoungmin <rudals1173@naver.com>
2023-06-21 09:45:52 +0900

    leet-04. Maximum Depth of Binary Tree

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-import collections
-import heapq
-
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v, w))
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
 
-        Q = [{0,k}]
-        dist = collections.defaultdict(int)
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                current_root = queue.popleft()
+                if current_root.left:
+                    queue.append(current_root.left)
+                if current_root.right:
+                    queue.append(current_root.right)
+        return depth
 
-        while Q:
-            time, node = heapq.heappop(Q)
-            if node not in dist:
-                dist[node] = time
-                for v, w in graph[node]:
-                    alt = time + w
-                    heapq.heappush(Q, (alt, v))
 
-        if len(dist) == n:
-            return  max(dist.values())
 
-        return -1
+
+
